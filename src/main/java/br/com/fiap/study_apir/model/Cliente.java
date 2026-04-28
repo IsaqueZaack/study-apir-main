cc562b1c3d677e6c142689398b508db6681b3fa6
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-27 22:32:02 -0300

    feat:interacoes com o banco de dados

---
+++ src/main/java/br/com/fiap/study_apir/model/Cliente.java
@@ -0,0 +1,26 @@
+package br.com.fiap.study_apir.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+
+@Entity
+@Table(name="clientes")
+public class Cliente {
+    @Id
+    private Long id;
+
+    @Column(name="nome_cliente", length = 100, 
+    columnDefinition ="char(100)", nullable = false)
+    private String nome;
+    
+    public Cliente() {
+
+    }
+}
